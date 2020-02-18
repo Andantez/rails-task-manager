0345af1e1af64b27abbbcb63ed706fdf9cef2424
Kaloyan Atansov <kaloyanatanasov83@gmail.com>
2020-02-18 12:25:52 +0000

    Adding a task router

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# List  all of the tasks
+# Add a task
+# View the details of the task
+# Edit a task
+# Remove a task
 class TasksController < ApplicationController
+  def index; end
 end
--- db/migrate/20200218115842_change_column_default_value.rb
+++ db/migrate/20200218115842_change_column_default_value.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Changing the default value of completed column
 class ChangeColumnDefaultValue < ActiveRecord::Migration[6.0]
   def change
     change_column_default :tasks, :completed, false
